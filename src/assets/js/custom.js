71d810cfaa1210fc41c45f693b220e7057b17be5
Ramiro Estrella <balance3840@hotmail.com>
2020-01-10 20:10:23 +0100

    Subiendo el header

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -8,7 +8,6 @@
 2. Set Header
 3. Init Menu
 4. Init Home Slider
-5. Init Google Map
 6. Init Testimonials Slider
 
 
@@ -29,7 +28,6 @@ $(document).ready(function()
 
 	initMenu();
 	initHomeSlider();
-	initGoogleMap();
 	initTestSlider();
 
 	setHeader();
@@ -137,80 +135,6 @@ $(document).ready(function()
 	}
 
 	/* 
-
-	5. Init Google Map
-
-	*/
-
-	function initGoogleMap()
-	{
-		var myLatlng = new google.maps.LatLng(40.760836, -73.910357);
-    	var mapOptions = 
-    	{
-    		center: myLatlng,
-	       	zoom: 14,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			draggable: true,
-			scrollwheel: false,
-			zoomControl: false,
-			zoomControlOptions:
-			{
-				position: google.maps.ControlPosition.RIGHT_CENTER
-			},
-			mapTypeControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			rotateControl: false,
-			fullscreenControl: false,
-			styles:
-			[
-			  {
-			    "featureType": "road.highway",
-			    "elementType": "geometry.fill",
-			    "stylers": [
-			      {
-			        "color": "#ffeba1"
-			      }
-			    ]
-			  }
-			]
-    	}
-
-    	// Initialize a map with options
-    	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-		// Re-center map after window resize
-		google.maps.event.addDomListener(window, 'resize', function()
-		{
-			setTimeout(function()
-			{
-				google.maps.event.trigger(map, "resize");
-				map.setCenter(myLatlng);
-			}, 1400);
-		});
-
-		function newLocation(newLat, newLng)
-		{
-			map.setCenter(
-			{
-				lat : newLat,
-				lng : newLng
-			});
-		}
-
-		var locationList = $('.location_contaner');
-		locationList.each(function()
-		{
-			var loc = $(this);
-			loc.on('click', function()
-			{
-				var newLat = loc.data('lat');
-				var newLng = loc.data('lng');
-				newLocation(newLat, newLng);
-			});
-				
-		});
-	}
 
 	/* 
 
