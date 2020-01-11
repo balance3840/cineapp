3059240e616041f676be3b1fdd3f9a8f17edb0c8
radhames tavera <23radhames@gmail.com>
2020-01-11 01:24:01 +0100

    mapping detalle pelicula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { DetallePeliculaComponent } from './components/detalle-pelicula/detalle-pelicula.component';
 
 const routes: Routes = [
   {'path': '', component: HomeComponent},
-  {'path': 'pelicula', component: PeliculaComponent}
+  {'path': 'pelicula', component: PeliculaComponent},
+  {'path': 'detallePelicula', component: DetallePeliculaComponent}
 ];
 
 @NgModule({
