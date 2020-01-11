b5af616dff3be0a2a4c8894af5879a7eba90f62e
Ramiro Estrella <balance3840@hotmail.com>
2020-01-11 23:30:57 +0100

    Redirecting

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { baseUrl } from '../shared/consts';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor(private http: HttpClient) { }
+  private subject = new Subject<any>();
+
+  constructor(private http: HttpClient) {}
 
   login(usuario: any): Observable<any> {
     return this.http.post<any>(baseUrl + 'usuarios/login', usuario).pipe();
+  }
+
+  isAuthenticatedObservable(): Observable<any> {
+    this.subject.next({ "auth": false });
+    if(localStorage.getItem("usuario")){
+      this.subject.next({ "auth": true });
+    }
+    return this.subject.asObservable();
   }
 
   isAuthenticated() {
