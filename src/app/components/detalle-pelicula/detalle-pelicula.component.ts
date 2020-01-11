24fc47faa36908b407ebee9654cb42ac39253e93
radhames tavera <23radhames@gmail.com>
2020-01-11 23:23:58 +0100

    cambios en servicios criticas y peliculas critias

--- src/app/components/detalle-pelicula/detalle-pelicula.component.ts
+++ src/app/components/detalle-pelicula/detalle-pelicula.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CriticaService } from 'src/app/services/critica.service';
+import { Critica } from 'src/app/models/critica';
+import { PeliculaService } from 'src/app/services/pelicula.service';
+import { Pelicula } from 'src/app/models/pelicula';
 
 @Component({
   selector: 'app-detalle-pelicula',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallePeliculaComponent implements OnInit {
 
-  constructor() { }
+  criticas: Critica[];
+  peliculasIds: number[];
+  peliculas : Pelicula;
+
+  constructor(private criticaService: CriticaService,private peliculaService:PeliculaService) { }
 
   ngOnInit() {
+    this.criticaService.getCriticas().subscribe(criticas => this.criticas = criticas );
+    // this.peliculaService.getPeliculasID(1).subscribe(peliculas => this.peliculas = peliculas);
   }
 
 }
---
+++ src/app/models/critica.ts
@@ -0,0 +1,6 @@
+export class Critica {
+    id: number;
+    criticaPelicula: string;
+    idUsuario: number;
+    idPelicula: number;
+}
