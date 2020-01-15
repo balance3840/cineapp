d9811551f5a8609da6bc3f57d8ece237e10262d7
vladimirtaveras <vladimirtaverasp@gmail.com>
2020-01-15 01:16:50 +0100

    Implementacion de validacion de Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,70 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-  usuario = {nombre: '', clave: ''};
+  erroresForm = {
 
-  constructor(private usuarioService: UsuarioService, private router: Router) { }
+    'nombre': '',
+
+    'clave': ''
+
+  };
+
+
+  mensajesError = {
+
+    'nombre': {
+      'required': 'El nombre de Inicio es obligatorio.',
+
+      'minlength': 'El nombre debe tener una longitud mínima de 2 caracteres.',
+
+      'maxlength': 'El nombre no puede exceder de 25 caracteres.'
+    },
+
+    'clave': {
+
+      'required': 'La Clave Fin son obligatorios.',
+
+      'minlength': 'La Clave debe tener una longitud mínima de 5 caracteres.',
+
+      'maxlength': 'La Clave no puede exceder de 25 caracteres.'
+    }
+  };
+
+  usuario = {nombre: '', clave: ''};
+  usuarioForm: FormGroup;
+  constructor(private usuarioService: UsuarioService, private router: Router,
+    private fb: FormBuilder) {this.crearFormulario(); }
 
   ngOnInit() {
     if(this.usuarioService.isAuthenticated()) {
       this.router.navigate(['dashboard']);    
+    }
+  }
+
+  crearFormulario() {
+    this.usuarioForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      clave: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]]
+    });
+
+    this.usuarioForm.valueChanges.subscribe(datos => this.onCambioValor(datos));
+    this.onCambioValor();
+
+  }
+
+  onCambioValor(data?: any) {
+    if (!this.usuarioForm) { return; }
+    const form = this.usuarioForm;
+    for (const field in this.erroresForm) {
+      // Se borrarán los mensajes de error previos
+      this.erroresForm[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.mensajesError[field];
+        for (const key in control.errors) {
+          this.erroresForm[field] += messages[key] + ' ';
+        }
+      }
     }
   }
 
